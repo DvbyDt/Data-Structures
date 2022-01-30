ba351173514c518779042e3c416d71eb56682593
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-01-30 15:55:21 +0530

    Time: 2 ms (24.04%), Space: 42.7 MB (7.11%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,23 +1,35 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        int [] temp = new int[nums.length];
-        // putting min values at the output array
-        for(int i=0;i<temp.length;i++){
-            temp[i] = Integer.MIN_VALUE;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(temp[index[i]] == Integer.MIN_VALUE){
-                temp[index[i]] = nums[i];
-            }else{
-                for(int j=index[i]+1;j<nums.length;j++){
-                    int temps = temp[j];
-                    temp[j] = temp[index[i]];
-                    temp[index[i]] = temps;
-                }
-            }
-            temp[index[i]] = nums[i];
-        }
-        return temp;
+        // int [] temp = new int[nums.length];
+        // // putting min values at the output array
+        // for(int i=0;i<temp.length;i++){
+        //     temp[i] = Integer.MIN_VALUE;
+        // }
+        // for(int i=0;i<nums.length;i++){
+        //     if(temp[index[i]] == Integer.MIN_VALUE){
+        //         temp[index[i]] = nums[i];
+        //     }else{
+        //         for(int j=index[i]+1;j<nums.length;j++){
+        //             int temps = temp[j];
+        //             temp[j] = temp[index[i]];
+        //             temp[index[i]] = temps;
+        //         }
+        //     }
+        //     temp[index[i]] = nums[i];
+        // }
+        // return temp;
+        
+        //Method-2
+	ArrayList<Integer> list = new ArrayList<>();
+    for (int i = 0; i <nums.length ; i++) {
+        list.add(index[i],nums[i]);
+//        list.add(index, element);
+    }
+    int e= 0;
+    for (Integer integer : list) {
+        nums[e++]=integer;
+    }
+    return nums;
         
     }
 }
