e7f1183a11b255ed5583cd24735302776c307c19
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-02-01 13:01:47 +0530

    Time: 2 ms (38.26%), Space: 41.6 MB (57.95%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+//         int[] temp = new int[nums.length];
+//         //Copying the elements to the array
+//         for(int i=0;i<temp.length;i++){
+//             temp[i] = nums[i];
+//         }
+//         //Now main operation
+//         for(int i=0;i<nums.length;i++){
+//             temp[i] = nums[nums[i]];
+//         }
+//         return temp;
+        int[] temp = new int[nums.length];
+		for(int i=0;i<nums.length;i++) {
+			temp[i] = nums[nums[i]];
+		}
+        System.gc();
+		return temp;       
+        
+    }
+}
