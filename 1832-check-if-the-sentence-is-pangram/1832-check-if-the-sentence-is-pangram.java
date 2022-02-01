c618f7af5caf2e7b043be79cbc7f91a814811fb8
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-02-01 12:56:46 +0530

    Time: 1 ms (87.80%), Space: 42.5 MB (5.00%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,21 +1,31 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-         if(sentence.length()==0) {
-			 return true;
-		 }
-		 if(sentence.length()==1) {
-			 return false;
-		 }
-		 int [] frequency_array = new int[256];
-		 for(int i=0;i<sentence.length();i++) {
-			 frequency_array[(int)sentence.charAt(i)] += 1;
-		 }
-		 for(int i=97;i<123;i++) {
-			 if(frequency_array[i]<1) {
-				 return false;
+//          if(sentence.length()==0) {
+// 			 return true;
+// 		 }
+// 		 if(sentence.length()==1) {
+// 			 return false;
+// 		 }
+// 		 int [] frequency_array = new int[256];
+// 		 for(int i=0;i<sentence.length();i++) {
+// 			 frequency_array[(int)sentence.charAt(i)] += 1;
+// 		 }
+// 		 for(int i=97;i<123;i++) {
+// 			 if(frequency_array[i]<1) {
+// 				 return false;
+// 			 }
+// 		 }
+		 
+// 		 return true;
+        
+        //Better solution
+        boolean panagram = true;
+		 for(char c='a';c<='z';c++) {
+			 if(!sentence.contains(String.valueOf(c))) {
+				 panagram = false;
+				 break;
 			 }
 		 }
-		 
-		 return true;
+		 return panagram;	
     }
 }
