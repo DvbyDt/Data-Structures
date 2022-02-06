f0761a7220cabf69f330dcb5b3fdb7b5c0d72348
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-02-06 19:05:55 +0530

    Time: 1 ms (49.58%), Space: 47.7 MB (9.81%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,13 +1,24 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
+        //This method is taking o(n^2).
+        // int sum=0;
+        // for(int i=0;i<mat.length;i++){
+        //     for(int j=0;j<mat[i].length;j++){
+        //         if((i==j)||(i+j==mat.length-1)){
+        //             sum=sum+mat[i][j];
+        //         }
+        //     }
+        // }
+        // return sum;        
+        
+        //A new method is required which will take o(n).
         int sum=0;
         for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[i].length;j++){
-                if((i==j)||(i+j==mat.length-1)){
-                    sum=sum+mat[i][j];
-                }
-            }
+            sum+=mat[i][i];//primary diagonal
+            sum+=mat[mat.length-1-i][i];//secondary diagonal 
         }
-        return sum;        
+        //if it is odd you might have added the center element twice
+        sum = mat.length%2 == 0 ? sum : sum-mat[mat.length/2][mat.length/2];
+        return sum;
     }
 }
