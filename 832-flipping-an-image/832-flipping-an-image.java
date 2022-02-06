5e8cccd7f51bd3f0d9a5011c7e016f833facde00
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-02-06 12:59:05 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (29.32%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.java
+++ 832-flipping-an-image/832-flipping-an-image.java
@@ -1,17 +1,31 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
-         int[][]temp = new int[image.length][image[0].length];
-	        for(int i=0;i<image.length;i++) {
-                int row = 0;
-	        	for(int j=image[i].length-1;j>=0;j--) {
-	        		if(image[i][j]==0) {
-	        			temp[i][row] = 1;
-	        		}else if(image[i][j]==1) {
-	        			temp[i][row] = 0;
-	        		}
-                    row++;
-	        	}
-	        }
-	        return temp;
+         // int[][]temp = new int[image.length][image[0].length];
+         // for(int i=0;i<image.length;i++) {
+         //        int row = 0;
+         // for(int j=image[i].length-1;j>=0;j--) {
+         // if(image[i][j]==0) {
+         // temp[i][row] = 1;
+         // }else if(image[i][j]==1) {
+         // temp[i][row] = 0;
+         // }
+         //            row++;
+         // }
+         // }
+         // return temp;
+        //method-2:Without taking a temporary array
+		 for(int i=0;i<image.length;i++) {
+			 for(int j=0;j<image[i].length;j++) {
+				 //reverse
+				 if((image[i].length-1-j)>j) {
+					 int temp = image[i][image[i].length-1-j];
+					 image[i][image[i].length-1-j] = image[i][j];
+					 image[i][j] = temp;
+				 }
+				 //Invert
+				image[i][j] = image[i][j] == 1 ? 0:1; 
+			 }
+		 }
+		 return image;
     }
 }
