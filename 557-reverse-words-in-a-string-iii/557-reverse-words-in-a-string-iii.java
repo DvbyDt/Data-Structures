fcfc8d8e42fccdc6dc9fca8901bb780437d794c0
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-15 23:01:13 +0530

    Time: 2020 ms (5.01%), Space: 738.8 MB (5.11%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -1,25 +1,39 @@
 class Solution {
-    public String reverseWords(String s) {
-        String str="";
-        int spaceIndex=0;
-        for(int i=0;i<s.length();i++){
-            int j=0;
-            if(s.charAt(i)==' '){
-                j=i-1;
-            }
-            else if(i==s.length()-1){
-                j=i;
-            }
-            if(s.charAt(i)==' ' || i==s.length()-1){
-                for(;j>=spaceIndex;j--){
-                    str+=s.charAt(j);
-                }
-                if(i!=s.length()-1){
-                    str+=" ";
-                }
-                spaceIndex=i+1;
-            }
+    public String reverseWords(String str) {
+        // String str="";
+        // int spaceIndex=0;
+        // for(int i=0;i<s.length();i++){
+        //     int j=0;
+        //     if(s.charAt(i)==' '){
+        //         j=i-1;
+        //     }
+        //     else if(i==s.length()-1){
+        //         j=i;
+        //     }
+        //     if(s.charAt(i)==' ' || i==s.length()-1){
+        //         for(;j>=spaceIndex;j--){
+        //             str+=s.charAt(j);
+        //         }
+        //         if(i!=s.length()-1){
+        //             str+=" ";
+        //         }
+        //         spaceIndex=i+1;
+        //     }
+        // }
+        // return str; 
+         
+        String [] arr = str.split(" ");
+        String str1="";
+        for(int i=0;i<arr.length;i++) {
+        	String word =arr[i];
+        	for(int j= word.length()-1;j>=0;j--) {
+        		str1+=word.charAt(j);
+        	}
+            if(i!=arr.length-1){
+                str1+=" ";
+            }      	
         }
-        return str;        
+        //System.out.println(str1);
+        return str1;
     }
 }
