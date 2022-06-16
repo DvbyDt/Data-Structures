0b227cbaa23308f8840c2d5e6f54187d78259e0e
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-16 22:35:07 +0530

    Time: 1 ms (49.98%), Space: 42.2 MB (26.89%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,13 +1,15 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String str="";
-        for(int i=0;i<address.length();i++){
-            if(address.charAt(i)=='.'){
-                str+="[.]";
-            }else{
-                str+=address.charAt(i);
-            }            
-        }
-        return str;
+        // String str="";
+        // for(int i=0;i<address.length();i++){
+        //     if(address.charAt(i)=='.'){
+        //         str+="[.]";
+        //     }else{
+        //         str+=address.charAt(i);
+        //     }            
+        // }
+        // return str;
+        
+        return address.replace(".","[.]");
     }
 }
