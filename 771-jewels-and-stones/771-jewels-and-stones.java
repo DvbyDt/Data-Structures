c5cdad242443ef7c8792855dbcee211b134f2b95
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-16 23:16:07 +0530

    Time: 1 ms (97.14%), Space: 42.3 MB (48.72%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,11 +1,16 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int c=0;
-        for(int i=0;i<stones.length();i++){
-            for(int j=0;j<jewels.length();j++){
-                if(stones.charAt(i)==jewels.charAt(j)){
-                    c++;
-                }
+        // for(int i=0;i<stones.length();i++){
+        //     for(int j=0;j<jewels.length();j++){
+        //         if(stones.charAt(i)==jewels.charAt(j)){
+        //             c++;
+        //         }
+        //     }
+        // }
+        for(char ch:stones.toCharArray()){
+            if(jewels.indexOf(ch)>-1){
+                c++;
             }
         }
         return c;
