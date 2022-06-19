c2a3e229672139a126e8a06bcff9a8dfa6fce857
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-19 20:56:45 +0530

    Time: 11 ms (32.88%), Space: 47.4 MB (54.44%) - LeetHub

---
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> cellsInRange(String s) {
+        int c=0,r=0;
+        //Number of columns
+        for(int i=(int)(s.charAt(0));i<=(int)s.charAt(3);i++){
+            c++;
+        }
+        //Number of rows
+        for(int i=(int)(s.charAt(1));i<=(int)s.charAt(4);i++){
+            r++;
+        }
+        int[][] arr = new int[r][c];
+        int ascii = (int)(s.charAt(0));
+        List<String> output = new ArrayList<String>();
+        //Actual stroing the values
+        for(int i=0;i<c;i++){
+            char val = s.charAt(1);
+            for(int j=0;j<r;j++){
+                output.add((char)ascii+""+val);
+                val++;
+            }
+            ascii++;
+        }
+        return output;
+    }
+}
