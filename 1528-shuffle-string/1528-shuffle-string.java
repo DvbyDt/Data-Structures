cff7c0338d134331eafdde04734f8c90ea4a5ebf
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-19 13:51:18 +0530

    Time: 2 ms (56.92%), Space: 44.7 MB (53.73%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -2,14 +2,21 @@ class Solution {
     public String restoreString(String s, int[] indices) {
         String output="";
         if(s.length()==indices.length){
+            // for(int i=0;i<indices.length;i++){
+            //     for(int j=0;j<indices.length;j++){
+            //         if(i==indices[j]){
+            //             output+=s.charAt(j);
+            //             break;
+            //         }
+            //     }
+            // }
+            char[] arr = new char[indices.length];
+            //Storing values in the char array
             for(int i=0;i<indices.length;i++){
-                for(int j=0;j<indices.length;j++){
-                    if(i==indices[j]){
-                        output+=s.charAt(j);
-                        break;
-                    }
-                }
-            }
+                arr[indices[i]] = s.charAt(i);
+            }            
+            return new String(arr);
+            
         }
         return output;
     }
