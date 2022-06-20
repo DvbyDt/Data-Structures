349a47533a0691bca373060d44e993a7e144de78
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-20 09:46:40 +0530

    Time: 4 ms (35.76%), Space: 40.6 MB (83.21%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -3,23 +3,32 @@ class Solution {
 	        String output="";
 	        String[] arr1 = s.split(" ");
 	        String[] arr2 = new String[arr1.length];
-	        for(int i=0;i<arr1.length;i++){
-	            int len = arr1[i].length();
-	            int temp = Integer.parseInt(String.valueOf(arr1[i].charAt(len-1)));
-	            arr2[temp-1] = arr1[i];
-	            //char temp = arr1[i].charAt(len-1);
-	            //arr2[temp-1] = arr2[temp-1].replace(arr1[i].charAt(len-1),"");
-	            //arr2[arr1[i].charAt(len-1)] = arr2[arr1[i].charAt(len-1)];
-	        }
-	        for(int i=0;i<arr2.length;i++){
-	        	String temp = arr2[i].substring(0,arr2[i].length()-1);
+	        // for(int i=0;i<arr1.length;i++){
+	        //     int len = arr1[i].length();
+	        //     int temp = Integer.parseInt(String.valueOf(arr1[i].charAt(len-1)));
+	        //     arr2[temp-1] = arr1[i];
+	        // }
+	        // for(int i=0;i<arr2.length;i++){
+	        // 	String temp = arr2[i].substring(0,arr2[i].length()-1);
+	        //     if(i==arr2.length-1){
+	        //         output+=temp;
+	        //     }else{
+	        //         output+=temp+" ";
+	        //     }            
+	        // }
+            for(int i=0;i<arr1.length;i++){
+                char temp = arr1[i].charAt(arr1[i].length()-1);
+                arr2[temp-'0'-1] = arr1[i].substring(0,arr1[i].length()-1);
+                
+            }
+         for(int i=0;i<arr2.length;i++){
+	        	//String temp = arr2[i].substring(0,arr2[i].length()-1);
 	            if(i==arr2.length-1){
-	                output+=temp;
+	                output+=arr2[i];
 	            }else{
-	                output+=temp+" ";
+	                output+=arr2[i]+" ";
 	            }            
 	        }
-	        
 	        return output;
 	    }
 }
