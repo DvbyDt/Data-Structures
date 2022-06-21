6564a8feb5dbf93f5844d7600e58bbc899f601df
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-21 13:39:34 +0530

    Time: 8 ms (23.23%), Space: 43.2 MB (18.34%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String output="";
+        if(s.length()>=1 && s.length()<=500){
+            String[] arr = s.split(" ");
+            for(int i=0;i<arr.length;i++){
+                if(i<k){
+                    if(i==k-1){
+                        output+=arr[i];
+                    }else{
+                        output+=arr[i]+" ";    
+                    }                    
+                }
+            }
+        }
+        return output;
+    }
+}
