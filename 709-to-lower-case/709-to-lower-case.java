4bf5d8d6d1a8e2e82bb13bad35d0e1e7d1bff54a
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-21 13:26:14 +0530

    Time: 13 ms (6.68%), Space: 42.8 MB (7.40%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,6 +1,16 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
-        
+        //return s.toLowerCase();
+        String output="";
+        for(int i=0;i<s.length();i++){
+            if((int)(s.charAt(i))>=65 && (int)(s.charAt(i))<=90){
+                int a = (int)(s.charAt(i)) - 65;
+                output+=(char)(97+a);
+            }
+            else{
+                output+=s.charAt(i);
+            }
+        }
+        return output;
     }
 }
