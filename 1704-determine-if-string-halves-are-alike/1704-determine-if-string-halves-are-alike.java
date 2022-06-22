c09e44db70ff40ad5be45eec802653ed7cd7496d
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-22 22:13:50 +0530

    Time: 133 ms (5.42%), Space: 42.4 MB (53.20%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,30 +1,65 @@
 class Solution {
-    public boolean halvesAreAlike(String s) {
-        // StringBuilder a1 = new StringBuilder();
-        // StringBuilder a2 = new StringBuilder();
+    public boolean halvesAreAlike(String s) 
+    {
+//         // StringBuilder a1 = new StringBuilder();
+//         // StringBuilder a2 = new StringBuilder();
+//         int left=0,right=0;
+//         for(int i=0;i<s.length();i++){
+//             if(i<(s.length()/2)){
+//                 // a1.append(s.charAt(i));
+//                 if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'
+//                   ||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'
+//                   ||s.charAt(i)=='O'||s.charAt(i)=='U'){
+//                     left++;
+//                 }
+//             }else{
+//                 //a2.append(s.charAt(i));
+//                 if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'
+//                   ||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'
+//                   ||s.charAt(i)=='O'||s.charAt(i)=='U'){
+//                     right++;
+//                 }
+//             }
+//         }
+        
+//         if(left==right){
+//             return true;
+//         }
+//         return false;
+        
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
         int left=0,right=0;
         for(int i=0;i<s.length();i++){
-            if(i<(s.length()/2)){
-                // a1.append(s.charAt(i));
-                if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'
-                  ||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'
-                  ||s.charAt(i)=='O'||s.charAt(i)=='U'){
-                    left++;
-                }
+            if(i<s.length()/2){
+                s1.append(s.charAt(i));
+                left = halves(s1.toString());
             }else{
-                //a2.append(s.charAt(i));
-                if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'
-                  ||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'
-                  ||s.charAt(i)=='O'||s.charAt(i)=='U'){
-                    right++;
-                }
+                s2.append(s.charAt(i));
+                right = halves(s2.toString());
             }
         }
-        
         if(left==right){
             return true;
         }
         return false;
-        
     }
+    
+    public static int halves(String s)
+    {
+        String s1 = "aeiouAEIOU";
+        char[] arr = s.toCharArray();
+        int count=0;
+        for(int i=0;i<arr.length;i++)
+        {
+           if(!(s1.indexOf(arr[i])!=-1))
+            {
+              count++;   
+            }
+             
+        }
+         return count;   
+        }
 }
+        
+        
