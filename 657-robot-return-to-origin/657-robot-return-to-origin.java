be60429f7730ff1fe6e269527f2843a2414d63ff
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-23 22:13:58 +0530

    Time: 7 ms (64.73%), Space: 45 MB (41.17%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.java
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -1,19 +1,30 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int moveup = 0,moveleft=0,moveright=0,movedown=0;
+        // int moveup = 0,moveleft=0,moveright=0,movedown=0;
+        // for(int i=0;i<moves.length();i++){
+        //     if(moves.charAt(i)=='L'){
+        //         moveleft++;
+        //     }else if(moves.charAt(i)=='U'){
+        //         moveup++;
+        //     }else if(moves.charAt(i)=='R'){
+        //         moveright++;
+        //     }
+        //     else{
+        //         movedown++;
+        //     }
+        // }
+        // if(moveup==movedown && moveleft == moveright){return true;}
+        // return false;
+        
+        //Solution-2
+        int[] arr = new int[26];
         for(int i=0;i<moves.length();i++){
-            if(moves.charAt(i)=='L'){
-                moveleft++;
-            }else if(moves.charAt(i)=='U'){
-                moveup++;
-            }else if(moves.charAt(i)=='R'){
-                moveright++;
-            }
-            else{
-                movedown++;
-            }
+            arr[moves.charAt(i) - 'A']++;
         }
-        if(moveup==movedown && moveleft == moveright){return true;}
+        if(arr['D' - 'A']==arr['U' - 'A'] && arr['L' - 'A'] == arr['R' - 'A']){
+            return true;
+        }
         return false;
+        
     }
 }
