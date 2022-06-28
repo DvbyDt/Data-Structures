ea23c990a42d9b485f2413a37c6a2c2b9854cc91
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-28 22:27:45 +0530

    Time: 1 ms (98.03%), Space: 42.4 MB (35.24%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.java
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.java
@@ -1,11 +1,18 @@
 class Solution {
     public int maxRepeating(String sequence, String word) {
-        StringBuilder sb = new StringBuilder(word);
-        int count=0;
-        while(sequence.contains(sb)){
-            count++;
-            sb.append(word);
+        // StringBuilder sb = new StringBuilder(word);
+        // int count=0;
+        // while(sequence.contains(sb)){
+        //     count++;
+        //     sb.append(word);
+        // }
+        // return count;
+        
+        //Just count repeating string with this simple method.
+        int ans=1;
+        while(sequence.contains(word.repeat(ans))){
+            ans++;
         }
-        return count;
+        return ans-1;
     }
 }
