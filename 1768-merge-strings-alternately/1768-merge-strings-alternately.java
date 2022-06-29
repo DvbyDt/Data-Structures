6ef4d2458a97b7442bbc88fe45ca8e293f389370
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-29 22:32:16 +0530

    Time: 1 ms (92.67%), Space: 42.5 MB (41.48%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder output = new StringBuilder();
+        int i=0,j=0;
+        while(i<word1.length() && j<word2.length())
+        {
+            output.append(word1.charAt(i));
+            output.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+        while(i<word1.length()){
+            output.append(word1.charAt(i));
+            i++;
+        }
+        while(j<word2.length()){
+            output.append(word2.charAt(j));
+            j++;
+        }
+        return output.toString();
+    }
+}
