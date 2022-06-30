00d6f1d4078737b517acc2038de3fda903f34694
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-06-30 23:07:25 +0530

    Time: 4 ms (28.26%), Space: 41.7 MB (71.06%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> input = new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                input.push(s.charAt(i));
+            }
+            else{      
+                if(input.size()!=0 && (s.charAt(i)==')' && input.peek()=='(' || s.charAt(i)==']' && input.peek()=='[' || s.charAt(i)=='}' && input.peek()=='{'))
+                {
+                   input.pop(); 
+                }
+                else{
+                    return false;
+                }
+                
+            }
+        }
+        return input.size() ==0 ? true : false;
+    }
+}
