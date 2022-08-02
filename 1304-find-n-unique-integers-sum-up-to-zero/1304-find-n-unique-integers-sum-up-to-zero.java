e0906baf81fff73969ff6056effd0f82beacc1bb
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-08-02 17:57:30 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (55.80%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -1,16 +1,26 @@
 class Solution {
     public int[] sumZero(int n) {
         //base case
-        if(n==1||n==0){
-            return new int[n];
+        // if(n==1||n==0){
+        //     return new int[n];
+        // }
+        // int[] arr = new int[n];
+        // int sum = 0;
+        // for(int i=0;i<n-1;i++){
+        //     arr[i] = i-n;
+        //     sum = sum+arr[i];
+        // }
+        // arr[arr.length-1] = Math.abs(sum);
+        // return arr;
+        
+        //Even better solution
+        //Just traverse half the array and place any number and it's -number 
+        int arr[] = new int[n];
+        for(int i=0;i<n/2;i++){
+            arr[i] = (n-i)*-1;
+            //This is from the back
+            arr[n-i-1] = (n-i);
         }
-        int[] arr = new int[n];
-        int sum = 0;
-        for(int i=0;i<n-1;i++){
-            arr[i] = i-n;
-            sum = sum+arr[i];
-        }
-        arr[arr.length-1] = Math.abs(sum);
         return arr;
     }
 }
