28965823cb8084462d7d2c03f21e8b7ce3573fb5
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-08-24 22:12:04 +0530

    Time: 9 ms (31.53%), Space: 64.7 MB (18.27%) - LeetHub

--- 885-spiral-matrix-iii/885-spiral-matrix-iii.java
+++ 885-spiral-matrix-iii/885-spiral-matrix-iii.java
@@ -1,69 +1,94 @@
 class Solution {
-    private String[] d = new String[]{"RIGHT", "DOWN", "LEFT", "UP"};
+    // private String[] d = new String[]{"RIGHT", "DOWN", "LEFT", "UP"};
     
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int rowSize = 1;
-        int colSize = 1;
+//         int rowSize = 1;
+//         int colSize = 1;
         
-        int p=rStart;
-        int q=cStart;
+//         int p=rStart;
+//         int q=cStart;
         
-        int[][] ret = new int[rows*cols][2];
-        int directionIndex = 0;
-        int counter = 0;
-        ret[counter++] = new int[]{p, q};
-        while(counter != rows*cols) {
-            String direction = d[directionIndex];
+//         int[][] ret = new int[rows*cols][2];
+//         int directionIndex = 0;
+//         int counter = 0;
+//         ret[counter++] = new int[]{p, q};
+//         while(counter != rows*cols) {
+//             String direction = d[directionIndex];
             
-            switch (direction) {
-                case "RIGHT":
-                    for (int i=0; i<colSize; i++) {
-                        if (isAvailable(p, ++q, rows, cols)) {
-                            ret[counter++] = new int[]{p, q};
-                        }
-                    }
-                    colSize++;
-                    break;
+//             switch (direction) {
+//                 case "RIGHT":
+//                     for (int i=0; i<colSize; i++) {
+//                         if (isAvailable(p, ++q, rows, cols)) {
+//                             ret[counter++] = new int[]{p, q};
+//                         }
+//                     }
+//                     colSize++;
+//                     break;
 					
-                case "DOWN":
-                    for (int i=0; i<rowSize; i++) {
-                        if (isAvailable(++p, q, rows, cols)) {
-                            ret[counter++] = new int[]{p, q};
-                        }
-                    }
-                    rowSize++;
-                    break;
+//                 case "DOWN":
+//                     for (int i=0; i<rowSize; i++) {
+//                         if (isAvailable(++p, q, rows, cols)) {
+//                             ret[counter++] = new int[]{p, q};
+//                         }
+//                     }
+//                     rowSize++;
+//                     break;
 					
-                case "LEFT":
-                    for (int i=0; i<colSize; i++) {
-                        if (isAvailable(p, --q, rows, cols)) {
-                            ret[counter++] = new int[]{p, q};
-                        }
-                    }
-                    colSize++;
-                    break;
+//                 case "LEFT":
+//                     for (int i=0; i<colSize; i++) {
+//                         if (isAvailable(p, --q, rows, cols)) {
+//                             ret[counter++] = new int[]{p, q};
+//                         }
+//                     }
+//                     colSize++;
+//                     break;
 					
-                case "UP":
-                    for (int i=0; i<rowSize; i++) {
-                        if (isAvailable(--p, q, rows, cols)) {
-                            ret[counter++] = new int[]{p, q};
-                        }
-                    }
-                    rowSize++;
-                    break;
+//                 case "UP":
+//                     for (int i=0; i<rowSize; i++) {
+//                         if (isAvailable(--p, q, rows, cols)) {
+//                             ret[counter++] = new int[]{p, q};
+//                         }
+//                     }
+//                     rowSize++;
+//                     break;
+//             }
+            
+//             directionIndex = (directionIndex+1) % 4;
+//         }
+        
+//         return ret;
+        int matrix[][] = new int[rows*cols][2];
+        int i=0;
+        //directions
+        int[] directions = {0,1,0,-1,0};
+        matrix[i++] = new int[]{rStart,cStart};
+        int d=0;
+        int len=0;
+        while(i<rows*cols){
+            if(d==0 || d==2){
+                len++;//East and West mai lenght badhti hai
             }
             
-            directionIndex = (directionIndex+1) % 4;
+            for(int k=0;k<len;k++){
+                rStart = rStart+directions[d];
+                cStart = cStart+directions[d+1];
+                
+                if(rStart>=0 && rStart<rows && cStart>=0 && cStart<cols){
+                    matrix[i++] = new int[]{rStart,cStart};
+                }
+            }
+            
+            d = ++d%4;
         }
         
-        return ret;
+        return matrix;
     }
     
-    private boolean isAvailable(int p, int q, int maxR, int maxC) {
-        if ( (0 <= p && p <= (maxR - 1)) && (0 <= q && q <= (maxC - 1))) {
-            return true;
-        }  
+//     private boolean isAvailable(int p, int q, int maxR, int maxC) {
+//         if ( (0 <= p && p <= (maxR - 1)) && (0 <= q && q <= (maxC - 1))) {
+//             return true;
+//         }  
         
-        return false;
-    }  
+//         return false;
+//     }  
 }
