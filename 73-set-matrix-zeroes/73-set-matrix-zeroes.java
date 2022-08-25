d5bf4d95e631b130a0c1910f5cad9bdefd3c6990
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-08-25 12:50:14 +0530

    Time: 1 ms (97.61%), Space: 54.4 MB (30.06%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,37 +1,86 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         //Brute-force approach
-        ArrayList<Integer> rows = new ArrayList<>();
-        ArrayList<Integer> cols = new ArrayList<>();
+//         ArrayList<Integer> rows = new ArrayList<>();
+//         ArrayList<Integer> cols = new ArrayList<>();
         
-        int n = matrix.length;
-        int m = matrix[0].length;
+//         int n = matrix.length;
+//         int m = matrix[0].length;
         
-        //Counting zeros
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+//         //Counting zeros
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(matrix[i][j]==0){
+//                     rows.add(i);
+//                     cols.add(j);
+//                 }
+//             }
+//         }
+        
+//         //Assigning Zeros
+//         //Rows:
+//         for(int i=0;i<rows.size();i++){
+//             for(int j=0;j<m;j++){
+//                 matrix[rows.get(i)][j] = 0;
+//             }
+//         }
+        
+//         //Columns:
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<cols.size();j++){
+//                 matrix[i][cols.get(j)] = 0;
+//             }
+//         }
+        
+        // return matrix;
+        
+        //Efficient approach:
+        int m=matrix.length;
+        int n=matrix[0].length;
+        boolean firstRow=false,firstCol=false;
+        
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0){
+                firstCol=true;
+                break;
+            }            
+        }
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){
+                firstRow=true;
+                break;
+            }        
+        }
+        
+        //Now make topmost and left 0
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    rows.add(i);
-                    cols.add(j);
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
         
-        //Assigning Zeros
-        //Rows:
-        for(int i=0;i<rows.size();i++){
-            for(int j=0;j<m;j++){
-                matrix[rows.get(i)][j] = 0;
+        //Now just filling zeros
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
             }
         }
         
-        //Columns:
-        for(int i=0;i<n;i++){
-            for(int j=0;j<cols.size();j++){
-                matrix[i][cols.get(j)] = 0;
+        if(firstRow){
+            for(int i=0;i<n;i++){
+                matrix[0][i] = 0;
             }
         }
         
-        // return matrix;
+        if(firstCol){
+            for(int i=0;i<m;i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
