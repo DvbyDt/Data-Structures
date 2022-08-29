82665deee3bb0f1bf7e5c8d4af17c46b553b5aec
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-08-29 22:23:40 +0530

    Time: 0 ms (100.00%), Space: 73.2 MB (38.40%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,18 +1,31 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         //Checking length
-        if(arr.length<3){return -1;}
-        int start=0,end=arr.length-1;
-        while(start<=end){
-            int mid = start+(end-start+1)/2;
-            if(arr[mid-1]<arr[mid] && arr[mid+1]<arr[mid]){
-                return mid;
-            }else if(arr[mid-1]<arr[mid]){
+        // if(arr.length<3){return -1;}
+        // int start=0,end=arr.length-1;
+        // while(start<=end){
+        //     int mid = start+(end-start+1)/2;
+        //     if(arr[mid-1]<arr[mid] && arr[mid+1]<arr[mid]){
+        //         return mid;
+        //     }else if(arr[mid-1]<arr[mid]){
+        //         start=mid+1;
+        //     }else {
+        //         end=mid-1;
+        //     }
+        // }
+        // return -1;
+        
+        //Approach-2
+        int start = 0,end=arr.length-1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(arr[mid]>arr[mid+1])//You are in the decreasing part
+            {
+                end=mid;
+            }else{
                 start=mid+1;
-            }else {
-                end=mid-1;
             }
         }
-        return -1;
+        return start;        
     }
 }
