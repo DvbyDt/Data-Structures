f9e467c685863a61e647efcb5054591f67026f21
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-08-30 18:18:10 +0530

    Time: 1 ms (87.69%), Space: 41.3 MB (75.64%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        //base case
+        if(m>nums.length){return 0;}
+        int max = 0 ,sum=0;
+        for(int i=0;i<nums.length;i++){
+            max = Math.max(max,nums[i]);
+            sum+=nums[i];
+        }
+        int ans=0;
+        int start = max , end = sum;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            //Checking if it is possible to split it
+            if(isPossibleDistribute(nums,m,mid)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    public static boolean isPossibleDistribute(int[] nums, int m, int mid){
+        int st = 1;//Number of students
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>mid){
+                sum = nums[i];
+                st++;
+            }
+        }
+        return st<=m;
+    }
+}
