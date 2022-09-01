29d41de787d0db93983123d9951f4cb3505de325
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-01 12:23:19 +0530

    Time: 3 ms (37.16%), Space: 41.5 MB (44.18%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,12 +1,30 @@
 class Solution {
     public int mySqrt(int x) {
-        //Appraoch-1: O(n) solution brute force
+        // //Appraoch-1: O(n) solution brute force
+        // if(x<2){return x;}
+        // long ans=1,i=1;
+        // for(;ans<=x;){
+        //     i++;
+        //     ans = i*i;
+        // }
+        // return (int)i-1;
+        
+        //Approach-2: Binary search
         if(x<2){return x;}
-        long ans=1,i=1;
-        for(;ans<=x;){
-            i++;
-            ans = i*i;
+        long start =1,end = x;
+        long ans = 0;
+        while(start<=end){
+            long mid = start+(end-start)/2;
+            if(mid*mid==x){
+                return (int)mid;                
+            }else if(mid*mid>x){
+                end = mid-1;
+            }else{
+                start = mid+1;
+                ans = mid;
+            }
         }
-        return (int)i-1;
+        return (int)ans;   
     }
+    
 }
