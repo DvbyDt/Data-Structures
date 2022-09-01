9ddaa296584d39e9d34c4fc63e8d845f4aef4c8a
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-01 13:58:49 +0530

    Time: 5 ms (17.43%), Space: 50.2 MB (44.24%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,14 +1,49 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        //Appraoch-1: O(n) solution
+        // int start = 0 , end = numbers.length-1;
+        // while(start<=end){
+        //     if(target==numbers[start]+numbers[end]){
+        //         return new int[]{start+1,end+1};
+        //     }else if(target<numbers[start]+numbers[end]){
+        //         end--;
+        //     }else{
+        //         start++;
+        //     }
+        // }
+        // return new int[]{-1,-1};
+        
+        //Approach-2: O(log n) binary search
         int start = 0 , end = numbers.length-1;
         while(start<=end){
-            if(target==numbers[start]+numbers[end]){
+            //Case 1: If start+end ==target
+            if(numbers[start]+numbers[end]==target){
                 return new int[]{start+1,end+1};
-            }else if(target<numbers[start]+numbers[end]){
-                end--;
-            }else{
-                start++;
             }
+            int mid = start + (end-start)/2;
+            //Case 2: If start+end <target
+            if(numbers[start]+numbers[end]<target){
+                //Case 2a: target > end+mid
+                if(numbers[end]+numbers[mid]<target){
+                    start = mid+1;
+                }
+                //Case 2b: target < mid+end
+                else{
+                    start++;
+                }
+            }
+            //Case 3: If start+end>target
+            else{
+                //Case 3a: If target < start+mid
+                if(numbers[start]+numbers[mid]>target){
+                    end = mid-1;
+                }
+                //Case 2b: target > mid+start
+                else{
+                    end--;
+                }
+            }
+            
         }
         return new int[]{-1,-1};
     }
