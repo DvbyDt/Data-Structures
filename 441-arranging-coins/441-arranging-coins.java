088e669ee473e8197c983845f943072086b3737a
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-01 18:51:36 +0530

    Time: 2 ms (96.94%), Space: 39.5 MB (97.27%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -2,11 +2,26 @@ class Solution {
     public int arrangeCoins(int n) {
         //Approach-1: Brute force
         if(n==0 || n==1) {return n;}
-        int res=1;
-        for(;res<=n;){
-            n = n-res;
-            res++;
+        // int res=1;
+        // for(;res<=n;){
+        //     n = n-res;
+        //     res++;
+        // }
+        // return res-1;
+        
+        //Appraoch-2:Binary Search
+        long start =1,end = n;
+        while(start<=end){
+            long mid = start+(end-start)/2;
+            long k = mid;
+            if((k*(k+1))/2 == n){
+                return (int)mid;
+            }else if((k*(k+1))/2 > n){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
         }
-        return res-1;
+        return (int)end;
     }
 }
