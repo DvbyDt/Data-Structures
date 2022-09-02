6f602bb118de7045cc283f58af0f0a374a778787
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-02 13:41:37 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (82.53%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -15,11 +15,25 @@ class Solution {
         //Appraoch-2: Since, Array is sorted and let's suppose array contains [1,2,3,4] and 
         // to find any missing element we do arr[idx] - (idx+1) i.e let's suppose idx = 1,
         //So, it becomes arr[1] - (1+1) = 2-2 =0. Hence, no element is missing
-        int idx = 0;
-        for(int i=0;i<arr.length && (arr[idx]-(idx+1))<k;){
-            idx = i+1;
-            i++;
+        // int idx = 0;
+        // for(int i=0;i<arr.length && (arr[idx]-(idx+1))<k;){
+        //     idx = i+1;
+        //     i++;
+        // }
+        // return k+idx;
+        
+        //Approach-3:O(log n) binary search solution.
+        int start=0,end = arr.length-1;
+        int closestmid = 0;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid]-(mid+1)<k){
+                start = mid+1;
+                closestmid = mid+1;
+            }else{
+                end = mid-1;
+            }
         }
-        return k+idx;
+        return k+closestmid;
     }
 }
