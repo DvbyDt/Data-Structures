fed5e8c34984e9eb4dabd6f123036fe4208bed9d
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-07 12:49:17 +0530

    Time: 642 ms (13.88%), Space: 56.3 MB (39.19%) - LeetHub

---
+++ 436-find-right-interval/436-find-right-interval.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        //Applying simple binary search to solve the problem
+        int[] ans = new int[intervals.length];
+        for(int i=0;i<intervals.length;i++){
+            int idx = binarySearch(intervals,intervals[i][1]);
+            ans[i] = idx;
+        }
+        return ans;
+    }
+    
+    public static int binarySearch(int[][] intervals,int target){
+        int minValue = Integer.MAX_VALUE;
+        int ans = -1;
+        for(int i=0;i<intervals.length;i++){
+            if(intervals[i][0]>=target && minValue>intervals[i][0]){
+                minValue = intervals[i][0];
+                ans = i;
+            }
+        }
+        return ans;
+    }
+}
