6e6a7d0a8d1c94aca2f82e13a2ee67db8784b57a
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-13 15:57:51 +0530

    Time: 25 ms (35.85%), Space: 60.5 MB (87.35%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,12 +1,34 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        //Approach-1: Compare first element with second and then return common
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]==nums[i]){
-                return nums[i];
+    public int findDuplicate(int[] nums) {        
+        // //Approach-1: Compare first element with second and then return common : O(nlogn)
+        // Arrays.sort(nums);
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i-1]==nums[i]){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        
+        //Approach-2: Pigenhole and binary search problem 
+        int start = 0,end  = nums.length-1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            int count = 0;
+            //Counting 
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]<=mid){
+                    count++;
+                }
             }
+            
+            if(count<=mid){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+         
         }
-        return -1;
+        return start;
+        
     }
 }
