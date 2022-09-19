7cca3b6f97f76b53aafb0d8b8f6e61b83a28b92f
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-19 23:25:49 +0530

    Time: 4 ms (59.79%), Space: 56.5 MB (29.59%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,6 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        //Approach-1:
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+        
+        //Approach-2: Moore's voting algo
+        int count=0,candidate = -1;
+        for(int n:nums){
+            if(count==0){
+                //count=count+1;
+                //Assigning the candidate a value other than -1.
+                candidate = n;
+            }
+            //If n == candidate count will increase else decrease
+            if(n==candidate){
+                count=count+1;
+            }else{
+                count=count-1;
+            }
+        }
+        return candidate;
     }
 }
