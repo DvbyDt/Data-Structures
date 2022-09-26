3b72504116317915296af3d25a558881e9102a0f
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-26 10:23:01 +0530

    Time: 2 ms (88.70%), Space: 55.6 MB (49.37%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,9 +1,27 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i] = nums[i]*nums[i];
+        //Approach-1: square and sort
+        // for(int i=0;i<nums.length;i++){
+        //     nums[i] = nums[i]*nums[i];
+        // }
+        // Arrays.sort(nums);
+        // return nums;
+        
+        //Approach-2: Maintain two pointers
+        int[] arr = new int[nums.length];
+        int i = 0,j = nums.length-1,k=nums.length-1;
+        while(i<=j)
+        {
+            if(Math.abs(nums[i])<=Math.abs(nums[j])){
+                arr[k] = nums[j]*nums[j];
+                k--;
+                j--;
+            }else{
+                arr[k] = nums[i]*nums[i];
+                k--;
+                i++;
+            }                
         }
-        Arrays.sort(nums);
-        return nums;
+        return arr;
     }
 }
