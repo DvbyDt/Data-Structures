a2842ebf7832c7ef0d003edeb1b649f9e0db43a8
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-09-28 07:57:25 +0530

    Time: 35 ms (24.53%), Space: 67.1 MB (19.00%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -7,19 +7,17 @@ class Solution {
             int j = i+1;
             List<Integer> ans = new ArrayList<>();
             if(j<arr.length){
-                if(arr[j]-arr[i]<min){
+                if(arr[j]-arr[i]<=min){
+                    if(arr[j]-arr[i]<min){
                     min = arr[j] - arr[i];
                     minDiff.clear();//To clear the previous record of min value as the first value
                     //will be entered regardless
+                    }                    
                     ans.add(arr[i]);
                     ans.add(arr[j]);
                     minDiff.add(ans);
                 }
-                else if(arr[j]-arr[i]==min){
-                    ans.add(arr[i]);
-                    ans.add(arr[j]);
-                    minDiff.add(ans);
-                }
+            
             }
         }
         return minDiff;
