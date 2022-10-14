23f866a073b4a9aa7749b5e9e9de225d803ac3bd
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-10-14 14:15:27 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (88.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,47 @@
 class Solution {
     public void sortColors(int[] nums) {
-        //Approach-1: Just sort the given nums using an technique.
-        //Approach-2: Count the number 
-        int count0=0,count1=0,count2=0;
-        for(int i=0;i<nums.length;i++){
+        // //Approach-1: Just sort the given nums using an technique.
+        // //Approach-2: Count the number 
+        // int count0=0,count1=0,count2=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         count0++;
+        //     }else if(nums[i]==1){
+        //         count1++;
+        //     }else {
+        //         count2++;
+        //     }
+        // }
+        // int k=0;
+        // //Now filling the arrays
+        // for(int i=0;i<count0;i++){
+        //     nums[k++] = 0;
+        // }
+        // for(int i=0;i<count1;i++){
+        //     nums[k++] = 1;
+        // }
+        // for(int i=0;i<count2;i++){
+        //     nums[k++] = 2;
+        // }
+        
+        //Approach-3: Taking two pinters approach
+        int i=0,zi=0,ti = nums.length-1;
+        while(i<=ti){
             if(nums[i]==0){
-                count0++;
-            }else if(nums[i]==1){
-                count1++;
-            }else {
-                count2++;
+                //Swap with zero Index
+                swap(nums,i++,zi++);
+            }else if(nums[i]==2){
+                //Swap with two index
+                swap(nums,i,ti--);
+            }else{
+                i++;
             }
         }
-        int k=0;
-        //Now filling the arrays
-        for(int i=0;i<count0;i++){
-            nums[k++] = 0;
-        }
-        for(int i=0;i<count1;i++){
-            nums[k++] = 1;
-        }
-        for(int i=0;i<count2;i++){
-            nums[k++] = 2;
-        }
+    }
+    
+    public static void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
