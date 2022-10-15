3982c594251495305e9691b29d83f1f0d9f91c2f
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-10-15 23:05:46 +0530

    Time: 5 ms (86.93%), Space: 44.6 MB (52.40%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -12,34 +12,68 @@ class Solution {
     public ListNode insertionSortList(ListNode head) {
       //APPROACH-1: BRUTE FORCE
       //base case
+        // if(head==null || head.next==null){return head;}
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     ListNode next = temp.next;
+        //     ListNode start = head;
+        //     ListNode prev = head;
+        //     //Now Compare
+        //     while(start!=next){
+        //         if(start.val>temp.val){
+        //             temp.next = start;//Point temp to start
+        //             if(start==head){//start is at the head
+        //                 head = temp;
+        //             }else{
+        //                 prev.next = temp;
+        //             }
+        //             //Moving start pointer
+        //             while(start.next!=temp){
+        //                 start = start.next;
+        //             }
+        //             //Pointing start to the right pointer
+        //             start.next = next;
+        //         }
+        //         //If less than the temp val
+        //         prev = start;
+        //         start = start.next;
+        //     }
+        //     temp = next;
+        // }
+        // return head;
+        
+        //APPROACH-2:O(1) approach
         if(head==null || head.next==null){return head;}
-        ListNode temp = head;
-        while(temp!=null){
-            ListNode next = temp.next;
-            ListNode start = head;
-            ListNode prev = head;
-            //Now Compare
-            while(start!=next){
-                if(start.val>temp.val){
-                    temp.next = start;//Point temp to start
-                    if(start==head){//start is at the head
-                        head = temp;
-                    }else{
-                        prev.next = temp;
+        ListNode prev = head;
+        ListNode curr = prev.next;
+        
+        while(curr!=null){
+            if(curr.val<prev.val){
+                prev.next = curr.next;
+                //Check if it is the head
+                if(curr.val<=head.val){
+                    curr.next = head;
+                    head = curr;
+                }else{
+                    //Check from the starting
+                    ListNode ptr = head;
+                    //Finding right position for the ptr
+                    while(ptr.next!=null && ptr.next.val < curr.val){
+                        ptr = ptr.next;
                     }
-                    //Moving start pointer
-                    while(start.next!=temp){
-                        start = start.next;
-                    }
-                    //Pointing start to the right pointer
-                    start.next = next;
+                    //Swapping
+                    ListNode temp = ptr.next;
+                    ptr.next = curr;
+                    curr.next = temp;
                 }
-                //If less than the temp val
-                prev = start;
-                start = start.next;
+                curr = prev.next;//Very important step this one. Remember the curr.next is changed.
+                //Hence, can't be used and prev is pointing to the right node.
+            }else{
+                curr = curr.next;
+                prev = prev.next;
             }
-            temp = next;
         }
+        
         return head;
     }
 }
