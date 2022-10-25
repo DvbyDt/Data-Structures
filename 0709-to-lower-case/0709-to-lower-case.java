f5ed7667669832821ac47c9287fe112cccef529c
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-10-25 12:56:43 +0530

    Time: 1 ms (83.63%), Space: 41.9 MB (71.74%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,17 +1,17 @@
 class Solution {
     public String toLowerCase(String s) {
         //Approach-1: SImple use of library
-        //return s.toLowerCase();
+        return s.toLowerCase();
         
         //Approach-2: Iterate in the loop and check for ASCII value if in upper case change it
-        StringBuilder sb =  new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)>64 && s.charAt(i)<91){
-                sb.append((char)(s.charAt(i)+32));
-            }else{
-                sb.append(s.charAt(i));
-            }
-        }
-        return sb.toString();
+        // StringBuilder sb =  new StringBuilder();
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)>64 && s.charAt(i)<91){
+        //         sb.append((char)(s.charAt(i)+32));
+        //     }else{
+        //         sb.append(s.charAt(i));
+        //     }
+        // }
+        // return sb.toString();
     }
 }
