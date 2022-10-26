f69fe3c398267ee9a29c295bec9d91b4aec2f603
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-10-26 13:02:48 +0530

    Time: 8 ms (73.92%), Space: 45.6 MB (12.96%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,20 +1,32 @@
 class Solution {
     public boolean judgeCircle(String moves) {
         //Approach-1:Take complimentary directions and quate them at the time of recording.
-        int moveL=0,moveR=0,moveD=0,moveU=0;
-        for(int i=0;i<moves.length();i++){
-            if(moves.charAt(i)=='U'){
-                moveU++;
-            }else if(moves.charAt(i)=='D'){
-                moveD++;
-            }else if(moves.charAt(i)=='L'){
-                moveL++;
-            }else{
-                moveR++;
-            }
+//         int moveL=0,moveR=0,moveD=0,moveU=0;
+//         for(int i=0;i<moves.length();i++){
+//             if(moves.charAt(i)=='U'){
+//                 moveU++;
+//             }else if(moves.charAt(i)=='D'){
+//                 moveD++;
+//             }else if(moves.charAt(i)=='L'){
+//                 moveL++;
+//             }else{
+//                 moveR++;
+//             }
+//         }
+        
+//         if(moveL==moveR && moveD==moveU){return true;}        
+//         return false;
+        
+        //Approach-2: Use the aplahbets and return the answer
+        int [] arr = new int[26];
+        char[] ch = moves.toCharArray();
+        for(char c:ch){
+            arr[c-'A']++;
         }
         
-        if(moveL==moveR && moveD==moveU){return true;}
+        if(arr['D'-'A']==arr['U'-'A'] && arr['L'-'A']==arr['R'-'A']){
+            return true;
+        }
         
         return false;
         
