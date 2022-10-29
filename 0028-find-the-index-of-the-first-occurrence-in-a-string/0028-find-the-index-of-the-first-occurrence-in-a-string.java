1b9cf71773d65446d0a02515f04cf601ec71bd3f
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-10-29 13:07:10 +0530

    Time: 3 ms (11.41%), Space: 41.9 MB (62.86%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,24 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        //Approach-1: SImplest idea
+        //return haystack.indexOf(needle);
+        
+        //Approach-2: If you are told that you can't use indexOf()
+        //Just make use of sliding window
+        //haystack-needle because to leave room for tail of the needle.
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            //Here in this case we are starting from back due to the reason of test cases as most 
+            //of them differ at the last or second last character.
+            for(int j=needle.length()-1;j>=0;j--){
+                if(haystack.charAt(i+j)!=needle.charAt(j)){
+                    break;
+                }
+                if(j==0){//If you reach j that means all the characters matched.
+                    return i;
+                }
+            }            
+        }
+        
+        return -1;
     }
 }
