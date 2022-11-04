e15080bce295cd92c5fed24869200ab199a38b1c
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-11-04 22:17:28 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (8.84%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str = s.split(" ");
-        return str[str.length-1].length();
+        //Appraoch-1
+        // String[] str = s.split(" ");
+        // return str[str.length-1].length();
+        
+        //Approach-2:
+        int length=0;
+        for (int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                length++;
+            }else{
+                //If it's a space then check if we already passed the last element
+                if(length>0){return length;}
+            }
+        }
+        return length;
     }
 }
