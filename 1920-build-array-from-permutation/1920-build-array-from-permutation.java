120c07d38ad9459064910869b304b8f00996dbaf
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-11-06 23:11:20 +0530

    Time: 190 ms (5.79%), Space: 54.4 MB (6.62%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] temp = new int[nums.length];
-        //Copying the elements to the array
-        // for(int i=0;i<temp.length;i++){
-        //     temp[i] = nums[i];
-        // }
-        //Now main operation
+        //Better solution for follow-up question
+        //Using o(1). The trick was to store two values in one place.
+        int n = nums.length;
         for(int i=0;i<nums.length;i++){
-            temp[i] = nums[nums[i]];
+            nums[i] = ((nums[nums[i]]%n)*n)+nums[i];// The formula behind this is a = a + b*n; where in b we have [nums[nums[i]%n]
+            //System.out.println(nums[i]);
         }
-//         return temp;
-		// int[] temp = new int[nums.length];
-		// for(int i=0;i<nums.length;i++) {
-		// 	temp[i] = nums[nums[i]];
-		// }
-        System.gc();
-		return temp;       
+        //To retrive a value
+        //System.out.println(".........");
+        for(int i=0;i<nums.length;i++){
+            nums[i] = nums[i]/n;//a = a/n is to get the original values and a = a%n is for getting the older values
+            System.out.println(nums[i]);
+        }
         
+        return nums;
     }
 }
