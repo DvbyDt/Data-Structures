7375a6d9b57afbc7fe6132446d41c41e34355ecb
dV/dt <68496657+DvbyDt@users.noreply.github.com>
2022-11-11 11:34:31 +0530

    Time: 19 ms (34.55%), Space: 46.2 MB (31.71%) - LeetHub

---
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canTransform(String start, String end) {
+//         if(!start.replace("X","").equals(end.replace("X",""))){return true;}
+//         if(start.length()<end.length()){return false;} 
+//         int stR =0,enR=0,enL=0,stL=0;
+//         for(int i=0;i<start.length();i++){
+//             if(start.charAt(i)=='R') stR++;
+//             if(end.charAt(i)=='R') enR++;
+//             if(start.charAt(i)=='L') stL++;
+//             if(end.charAt(i)=='L') enL++;
+//             if(enR>stR || stL>enL) return false;
+//         }
+        
+//         return true;
+        String startModified = start.replace("X", "");
+    String endModified = end.replace("X", "");
+    if (!startModified.equals(endModified)) return false;
+    int stR = 0, enR = 0, stL = 0, enL = 0;
+    for (int i = 0; i < start.length(); i++) {
+      if (start.charAt(i) == 'R') stR++;
+      if (end.charAt(i) == 'R') enR++;
+      if (start.charAt(i) == 'L') stL++;
+      if (end.charAt(i) == 'L') enL++;
+      if (enR > stR || stL > enL) return false;
+    }
+    return true;
+    }
+}
